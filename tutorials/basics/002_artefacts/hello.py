0d1a6e292ed2a8f4a183b57f0758751c6e3aca5b
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-07-05 09:33:31 +0200

    Add tutorial for Python usage

---
+++ tutorials/basics/002_artefacts/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import argparse
+
+arg_parser = argparse.ArgumentParser(description='Say hello')
+arg_parser.add_argument('name', help='Name to greet')
+options = arg_parser.parse_args()
+
+print('Hello ' + options.name + '!')
---
+++ tutorials/basics/002_artefacts/sin_plot.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-2*np.pi, 2*np.pi, 501)
+y = np.sin(x)
+
+plt.plot(x, y, '-')
+plt.savefig('sin.png')
